ce4b31871abf46d8ced9777385afca337e94dd56
Baltej Singh <baltej963@gmail.com>
2025-01-02 17:52:23 +0530

    Changes done

--- src/App.d.ts
+++
@@ -1,5 +0,0 @@
-declare module './App.jsx' {
-  import { ComponentType } from 'react';
-  const App: ComponentType;
-  export default App;
-}
---
+++ src/comps/Navbar.jsx
@@ -0,0 +1,8 @@
+import { Button } from "@/components/ui/Button"
+export default function Navbar(){
+    return(
+        <>
+            <Button>NavItem</Button>
+        </>
+    )
+}
