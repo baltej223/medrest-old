df722fc684cc94eb2c24b74d8f82284488249a43
Baltej Singh <baltej963@gmail.com>
2025-01-02 17:41:57 +0530

    Some bugs fixed

--- src/App.d.ts
+++ src/App.d.ts
@@ -1,5 +1,5 @@
 declare module './App.jsx' {
-    import { ComponentType } from 'react';
-    const App: ComponentType;
-    export default App;
-  }
+  import { ComponentType } from 'react';
+  const App: ComponentType;
+  export default App;
+}
